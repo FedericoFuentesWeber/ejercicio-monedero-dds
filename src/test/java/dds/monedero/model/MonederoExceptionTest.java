b3d192068f8f1ffa4a7c6257e56fdf3f78894716
FedericoFuentesWeber <fedefuentes2112@gmal.com>
2022-05-12 21:54:54 -0300

    Refactor test

---
+++ src/test/java/dds/monedero/model/MonederoExceptionTest.java
@@ -0,0 +1,56 @@
+package dds.monedero.model;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import dds.monedero.exceptions.MaximaCantidadDepositosException;
+import dds.monedero.exceptions.MaximoExtraccionDiarioException;
+import dds.monedero.exceptions.MontoNegativoException;
+import dds.monedero.exceptions.SaldoMenorException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MonederoExceptionTest {
+
+  private Cuenta cuenta;
+
+  @BeforeEach
+  void init() {
+    cuenta = new Cuenta();
+  }
+
+  @Test
+  void PonerMontoNegativo() {
+    assertThrows(MontoNegativoException.class, () -> cuenta.poner(-1500));
+  }
+
+  @Test
+  void MasDeTresDepositos() {
+    assertThrows(MaximaCantidadDepositosException.class, () -> {
+      cuenta.poner(1500);
+      cuenta.poner(456);
+      cuenta.poner(1900);
+      cuenta.poner(245);
+    });
+  }
+
+  @Test
+  void ExtraerMasQueElSaldo() {
+    assertThrows(SaldoMenorException.class, () -> {
+      cuenta.setSaldo(90);
+      cuenta.sacar(1001);
+    });
+  }
+
+  @Test
+  public void ExtraerMasDe1000() {
+    assertThrows(MaximoExtraccionDiarioException.class, () -> {
+      cuenta.setSaldo(5000);
+      cuenta.sacar(1001);
+    });
+  }
+
+  @Test
+  public void ExtraerMontoNegativo() {
+    assertThrows(MontoNegativoException.class, () -> cuenta.sacar(-500));
+  }
+}
